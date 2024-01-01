90d1cc766a91162826f0207093debaa5f5cf4bd3
Zouhair055 <dkhissizouhair5@gmail.com>
2024-01-01 19:37:40 +0100

    edits0

--- routes/assignments.js
+++ routes/assignments.js
@@ -1,15 +1,38 @@
 //routes-assignement.js
-let Assignment = require('../model/assignment');
+const assignment = require('../model/assignment');
+let mongoose = require('mongoose');
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+let Shema = mongoose.Schema;
+
+let assignmentShema = Shema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+
+assignmentShema.plugin(aggregatePaginate);
+
+let Assignment = mongoose.model('assignment', assignmentShema);
+
+module.exports = Assignment;
 
 // Récupérer tous les assignments (GET)
 function getAssignments(req, res){
-    Assignment.find((err, assignments) => {
-        if(err){
-            res.send(err)
-        }
-
-        res.send(assignments);
-    });
+    var aggregateQuery = Assignment.aggregate();
+    Assignment.aggregatePaginate(
+        aggregateQuery, 
+        { 
+            page: parseInt(req.query.page) || 1, 
+            limit: parseInt(req.query.limit) || 10, 
+        },
+        (err, assignments) => {
+            if(err){
+                res.send(err);
+            }
+            res.send(assignments);
+        },
+    );
 }
 
 // Récupérer un assignment par son id (GET)
